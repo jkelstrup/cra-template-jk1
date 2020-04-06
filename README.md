7762068f2894ac208f5feb9ee476c35475db8a8f
Jonas Kelstrup <jonaskelstrup@gmail.com>
2020-04-06 10:36:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+JK1 Template
+======================
+`cra-template-jk1`
+
+What does this template do?
+
+- It includes some basic styling in the `index.html` file:
+  - Setting `box-sizing` to `border-box`
+  - Sets a System Font Stack
+  - Making sure `#root` fills the viewport width and height
+- It includes `styled-components`
+- It includes `gh-pages` for deployment
+- It uses `jsconfig.json` to facilitate absolute imports from the `src`-folder
+- It uses the following file structure:
+
+```
+> public/
+  • favicon.ico
+  • index.html
+  • logo192.png
+  • logo512.png
+  • manifest.json
+  • robots.txt
+> src/
+  > components/
+    > App/
+      • App.js
+      • App.test.js
+  > img/
+    • logo.svg
+  > misc/
+    • serviceWorker.js
+    • setupTests.js
+  • index.js
+• .gitignore
+• jsconfig.json
+• package.json
+• README.md
+```
---
+++ template/src/components/App/App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import styled, { keyframes } from 'styled-components/macro';
+import logo from 'img/logo.svg';
+
+const App = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${appLogoSpin} infinite 20s linear;
+  }
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
+
+const Code = styled.code`
+  font-family: SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace;
+`;
+
+export default function() {
+  return (
+    <App>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
+        <p>
+          Edit <Code>src/components/App/App.js</Code> and save to reload.
+        </p>
+        <AppLink
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </AppLink>
+      </AppHeader>
+    </App>
+  );
+}
